591fa0cea525cba44cd838a3c84d6c5806e73a07
Ferdinand Bada <ferdinandkamata@duck.com>
2024-07-25 13:50:13 +0300

    add navigator and fix android bug

--- App.tsx
+++ App.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import {SafeAreaView, useColorScheme} from "react-native";
 import {Colors} from "react-native/Libraries/NewAppScreen";
 import {MovieScreen} from "./src/screens/MovieScreen";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {NavigationContainer} from "@react-navigation/native";
+
+const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === "dark";
@@ -12,9 +16,15 @@ function App(): React.JSX.Element {
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <MovieScreen />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Discover-Movies"
+          component={MovieScreen}
+          options={{title: "Discover Movies"}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- src/screens/MovieScreen.tsx
+++ src/screens/MovieScreen.tsx
@@ -23,6 +23,5 @@ export const MovieScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
   },
 });
